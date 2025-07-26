60376931c9866c684a65a528c4e45cfaee370f8f
studentzzang <studentzzang@gmail.com>
2025-07-26 23:12:32 +0900

    Feat: Get user Info

--- src.py
+++ src.py
@@ -8,15 +8,29 @@ import time
 load_dotenv()
 
 _api_key = os.getenv("API_KEY")
-_api_secret = os.getenv("API_KEY_SERCRET")
+_api_secret = os.getenv("API_KEY_SECRET")
 
 session = HTTP(api_key = _api_key, api_secret = _api_secret)
+
+# ------ SETTING LINE ----------------
 
 # 가져올 코인
 coin_name = "DOGEUSDT"
 
 # interval 분봉가져옴 1=1min
 interval = "1"
+
+# ------  ---------------- -----------
+
+
+
+# get USER INFO
+balance_info = session.get_wallet_balance(accountType="UNIFIED")["result"]["list"][0] # 전체 자산(USD 기준)
+total_balance_usd = balance_info["totalAvailableBalance"]
+
+
+print(f"자산: {total_balance_usd}$ (USD)")
+
 
 while True:
     try:
@@ -31,7 +45,7 @@ while True:
         minute = datetime.now().minute
         sec = datetime.now().second
         
-        print(f"[{date} {hour} : {minute} : {sec}] {coin_name} 가격 {price}")
+        print(f"[{date} {hour}:{minute}:{sec}] {coin_name} 가격 {price}$")
         
     except:
         print("# # 에러 # #")
